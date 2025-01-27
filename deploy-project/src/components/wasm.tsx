a301f2442d934acfd66a4c42a39c2524aba71362
Amo Taiki <c1101081@planet.kanazawa-it.ac.jp>
2025-01-27 16:15:45 +0900

    先に手動で追加する力技を使う

--- deploy-project/src/components/wasm.tsx
+++ deploy-project/src/components/wasm.tsx
@@ -1,18 +1,31 @@
-import { useRef } from 'react'
+import { useLayoutEffect } from 'react'
 import init from "../graphics/wasm/pkg/rust_pentagram";
-import { useInView } from '../hooks/useInView';
+// import { useInView } from '../hooks/useInView';
 
 function Wasm_View() {
-    const statsRef = useRef<HTMLDivElement>(null)
-    let exist = useInView(statsRef)
-    if(exist) {
-        init()
-    }
+    useLayoutEffect(() => {
+        // statsというidを持つ要素を追加
+        const statsEl = document.createElement("div")
+        statsEl.id = "stats"
+
+        // containerというidを持つ要素に対してstatsElを追加
+        const containerEl = document.getElementById("container")
+        containerEl?.appendChild(statsEl)
+
+        // WebAssemblyの初期化
+        init().then(() => {
+            console.log("Wasm initialized")
+        })
+
+        // コンポーネントがアンマウントされた時にstatsElを削除
+        return () => {
+            containerEl?.removeChild(statsEl)
+        }
+    }, [])
     return (
         <main>
             <section id="container">
                 {/**WebAssemblyが描画する対象 */}
-                <div id="stats" ref={statsRef} />
             </section>
         </main>
     )
