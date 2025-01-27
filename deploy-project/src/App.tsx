99f717628cfca42ecfa45eb006f4990044034b1b
Amo Taiki <c1101081@planet.kanazawa-it.ac.jp>
2025-01-27 10:32:23 +0900

    rust_pentagramの初期化をpkgモジュールからの呼び出しに変更

--- deploy-project/src/App.tsx
+++ deploy-project/src/App.tsx
@@ -1,12 +1,12 @@
 import { useEffect } from 'react'
 import complete from "./graphics/typescript/animate"
-import {run} from "./graphics/wasm/rust_pentagram.js";
+import init from "./graphics/wasm/pkg";
 
 function App() {
   useEffect(() => {
     (async () => {
-      await complete()
-      run()
+      await complete();
+      (await init()).run();
     })()
   }, [])
 
