f976728ed7aa1db5163c60ee8c9bfed0373548b1
Amo Taiki <c1101081@planet.kanazawa-it.ac.jp>
2025-01-27 15:57:42 +0900

    難儀なコードはいらない

--- deploy-project/src/components/wasm.tsx
+++ deploy-project/src/components/wasm.tsx
@@ -4,30 +4,12 @@ import init from "../graphics/wasm/pkg/rust_pentagram";
 function Wasm_View() {
     useEffect(() => {
         const targetSelector = "#stats"
-        const observerCallback = (mutationsList: MutationRecord[], _observer: MutationObserver) => {
-            for(const mutation of mutationsList) {
-                if(mutation.type === "childList") {
-                    const targetEl = document.querySelector(targetSelector)
-                    if(targetEl) {
-                        init().then(() => {
-                            console.log("Wasm initialized")
-                        })
-                    }
-                }
-            }
-        }
-
-        const observer = new MutationObserver(observerCallback)
-        observer.observe(document.body, {childList: true, subtree: true})
-
         const initialCheck = document.querySelector(targetSelector)
         if(initialCheck) {
             init().then(() => {
                 console.log("Wasm initialized")
             })
         }   
-
-        return () => observer.disconnect()
     }, [])
 
     return (
