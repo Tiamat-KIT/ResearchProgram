16da3b0b18a893b56ebd592e21302e03389f4a32
Amo Taiki <c1101081@planet.kanazawa-it.ac.jp>
2025-01-27 10:15:55 +0900

    rust_pentagram.jsのインポートをrust_pentagramに変更

--- deploy-project/src/App.tsx
+++ deploy-project/src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import complete from "./graphics/typescript/animate"
-import init from "./graphics/wasm/rust_pentagram.js";
+import init from "./graphics/wasm/rust_pentagram";
 
 function App() {
   useEffect(() => {
