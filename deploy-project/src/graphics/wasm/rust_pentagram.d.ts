a75a3fa0928f0bcb7329b79d04037d3689fa5506
Amo Taiki <c1101081@planet.kanazawa-it.ac.jp>
2025-02-08 13:16:18 +0900

    昨夜の作業を保存

---
+++ deploy-project/src/graphics/wasm/rust_pentagram.d.ts
@@ -0,0 +1,55 @@
+/* tslint:disable */
+/* eslint-disable */
+export function run(): Promise<void>;
+export class FrameStats {
+  free(): void;
+  constructor();
+  update(frame_time: number): void;
+  average_time(): number;
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly __wbg_framestats_free: (a: number, b: number) => void;
+  readonly framestats_new: () => number;
+  readonly framestats_update: (a: number, b: number) => void;
+  readonly framestats_average_time: (a: number) => number;
+  readonly run: () => void;
+  readonly __wbindgen_export_0: () => number;
+  readonly __wbindgen_export_1: WebAssembly.Table;
+  readonly __wbindgen_export_2: (a: number) => void;
+  readonly __wbindgen_export_3: (a: number, b: number) => number;
+  readonly __wbindgen_export_4: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_export_5: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_export_6: WebAssembly.Table;
+  readonly closure6_externref_shim: (a: number, b: number, c: any, d: any) => void;
+  readonly closure4_externref_shim: (a: number, b: number, c: any) => void;
+  readonly closure190_externref_shim: (a: number, b: number, c: any) => void;
+  readonly __wbindgen_export_10: (a: number, b: number) => void;
+  readonly closure279_externref_shim: (a: number, b: number, c: any) => void;
+  readonly closure580_externref_shim: (a: number, b: number, c: any) => void;
+  readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ deploy-project/src/graphics/wasm/rust_pentagram_bg.wasm.d.ts
@@ -0,0 +1,22 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_framestats_free: (a: number, b: number) => void;
+export const framestats_new: () => number;
+export const framestats_update: (a: number, b: number) => void;
+export const framestats_average_time: (a: number) => number;
+export const run: () => void;
+export const __wbindgen_export_0: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_export_2: (a: number) => void;
+export const __wbindgen_export_3: (a: number, b: number) => number;
+export const __wbindgen_export_4: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export_5: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const closure6_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const closure4_externref_shim: (a: number, b: number, c: any) => void;
+export const closure190_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_export_10: (a: number, b: number) => void;
+export const closure279_externref_shim: (a: number, b: number, c: any) => void;
+export const closure580_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
